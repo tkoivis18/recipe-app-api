6e3a26fe9030c528a4e154384ba7282c1b5b66fa
tkoivis18 <tobias.koivisto@gmail.com>
2023-02-14 01:18:09 +0200

    added time.sleep to wait_for_db.py

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,5 +1,5 @@
 """
- Django command to wair for the database to be available.
+Django command to wair for the database to be available.
 """
 
 import time
@@ -8,6 +8,7 @@ from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
     """Django command to wait for database."""
@@ -22,4 +23,6 @@ class Command(BaseCommand):
                 db_up = True
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+
         self.stdout.write(self.style.SUCCESS('Database available!'))
