593c5e74a90cfc61e458fe3e2807d6e5e230bf65
tkoivis18 <tobias.koivisto@gmail.com>
2023-02-14 01:20:18 +0200

    fixing testcommand

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,7 +1,6 @@
 """
 Test custom Django management commands.
 """
-
 from unittest.mock import patch
 
 from psycopg2 import OperationalError as Psycopg2OpError
@@ -10,11 +9,12 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
 
-    def test_wait_for_db_reay(self, patched_check):
+    def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
 
@@ -27,7 +27,7 @@ class CommandTests(SimpleTestCase):
         """Test waiting for database when getting OperationalError."""
         patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
-        
+            
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
