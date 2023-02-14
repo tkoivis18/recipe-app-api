96a781b44f29f8d390533e2526c3b83b2ecc7da0
tkoivis18 <tobias.koivisto@gmail.com>
2023-02-14 21:50:07 +0200

    fixed commas

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -26,7 +26,7 @@ class ModelTests(TestCase):
         sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
             ['Test2@Example.com', 'Test2@example.com'],
-            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
             ['test4@example.COM', 'test4@example.com'],
         ]
         for email, expected in sample_emails:
@@ -41,7 +41,7 @@ class ModelTests(TestCase):
     def test_create_superuser(self):
         """Test creating a superuser."""
         user = get_user_model().objects.create_superuser(
-            'test@example.com'
+            'test@example.com',
             'test123',
         )
 
