9a4c596dc63aeac87742d8e0863f8b9ff846c8fe
tkoivis18 <tobias.koivisto@gmail.com>
2023-02-15 16:25:21 +0200

    created test for bad credentials

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -80,3 +80,13 @@ class PublicUserApiTests(TestCase):
 
         self.assertIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_bad_credentials(self):
+        """Test return error if credentials invalid"""
+        create_user(email='test@example.com', password='goodpass')
+
+        payload = {'email': 'password': 'badpass'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIN('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
