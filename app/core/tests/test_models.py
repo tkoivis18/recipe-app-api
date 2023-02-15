59689f7d17cc29ef2de3c2ac8647555136b40482
tkoivis18 <tobias.koivisto@gmail.com>
2023-02-15 18:43:20 +0200

    created recipe migrations

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -61,7 +61,7 @@ class ModelTests(TestCase):
         recipe = models.Recipe.objects.create(
             user=user,
             title='Sample recipe name',
-            time_minuts=5,
+            time_minutes=5,
             price=Decimal('5.50'),
             description='Sample recipe description.',
         )
