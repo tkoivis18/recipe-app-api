a6b91a637bd4b0dd6479024f6682b150b677949e
tkoivs18 <tobias.koivisto@gmail.com>
2023-02-17 14:02:10 +0200

    test for creating an ingredient

--- app/core/models.py
+++ app/core/models.py
@@ -56,6 +56,7 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
+    ingredients = models.ManyToManyField('Ingredient')
 
     def __str__(self):
         return self.title
@@ -70,3 +71,11 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+class Ingredient(models.Model):
+    """Ingredient for recipes"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
